28d5bc243ad5d55d209ed0e1aa06271b4a542d4b
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-19 13:28:54 +1000

    Added 20 Items to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+puts "Destroying Users"
+User.destroy_all
+puts "Creating Users"
 2.times do |i|
     User.create(email: "user-#{i+1}@example.com", password: "password", password_confirmation: "password")
 end
+
+puts "Creating Todo Items"
+User.all.each do |u|
+    10.times do |i|
+        u.todo_items.create(title: "To Do Item #{i+1} for #{u.email}", complete: i % 3  == 0 ? true : false  )
+    end
+end
+
+puts "Finished Seeding"
