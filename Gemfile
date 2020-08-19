d9d20fd46edec2937595cdf130cbb0a008617843
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-19 13:15:42 +1000

    Added Devise views and style

--- Gemfile
+++ Gemfile
@@ -54,5 +54,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Custom Added Gems
-
 gem 'devise'
+
+gem 'htmlbeautifier'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+      root "pages#my_todo_items", as: :authenticated_root
+  end
   root to: 'pages#home'
-  get 'pages/my_todo_items'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
