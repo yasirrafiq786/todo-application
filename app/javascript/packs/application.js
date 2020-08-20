c7cc0af22c39e69d7ddf83f3d7df65f74d43551d
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-20 17:52:33 +1000

    Added React files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,6 @@ require('channels');
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
+require('./index');
 require('bootstrap');
 import 'bootstrap/dist/css/bootstrap';
---
+++ app/javascript/packs/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>My First Rails-React App</div>;
+};
+
+export default App;
