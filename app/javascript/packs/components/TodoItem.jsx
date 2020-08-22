5992e97748d09a6fcccee7ecdf24d1db012307a6
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-22 15:48:51 +1000

    Made basic form and table

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,14 +1,56 @@
 import React, {useState} from 'react';
 
 const TodoItem = props => {
-    
+  const [done, setDone] = useState (props.complete);
 
   return (
-    <div>
-      <div>{props.userId}</div>
-      <div>{props.title}</div>
-      <div>{props.completed}</div>
-    </div>
+    <tr className={`${done ? 'table-light' : ''}`}>
+      <td>
+        <svg
+          className={`bi bi-check-circle ${done ? `text-success` : `text-muted`}`}
+          width="2em"
+          height="2em"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M17.354 4.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3-3a.5.5 0 11.708-.708L10 11.293l6.646-6.647a.5.5 0 01.708 0z"
+            clipRule="evenodd"
+          />
+          <path
+            fillRule="evenodd"
+            d="M10 4.5a5.5 5.5 0 105.5 5.5.5.5 0 011 0 6.5 6.5 0 11-3.25-5.63.5.5 0 11-.5.865A5.472 5.472 0 0010 4.5z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </td>
+      <td>
+        <input
+          type="text"
+          defaultValue={props.title}
+          disabled={done}
+          className="form-control"
+          id={`todoItem__title-${props.id}`}
+        />
+      </td>
+      <td className="text-right">
+        <div className="form-check form-check-inline">
+          <input
+            type="boolean"
+            defaultChecked={done}
+            type="checkbox"
+            className="form-check-input"
+            id={`complete-${props.id}`}
+          />
+          <label className="form-check-label" htmlFor={`complete-${props.id}`}>
+            Complete?
+          </label>
+        </div>
+        <button className="btn btn-outline-danger">Delete</button>
+      </td>
+    </tr>
   );
 };
 
--- app/javascript/packs/components/TodoList.jsx
+++ app/javascript/packs/components/TodoList.jsx
@@ -6,16 +6,35 @@ const TodoList = () => {
   const [items, setItems] = useState ([]);
 
   useEffect (async () => {
-    const response = await itemsList.get ("/todo_items");
-    setItems(response.data);
+    const response = await itemsList.get ('/todo_items');
+    setItems (response.data);
   }, []);
 
   return (
-    <div>
-
-      <TodoItem userId="User" title="Sample" completed="No" />
-
-    </div>
+    <React.Fragment>
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Status</th>
+              <th scope="col">Item</th>
+              <th scope="col" className="text-right">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map (item => (
+              <TodoItem
+                key={item.id}
+                title={item.title}
+                complete={item.complete}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </React.Fragment>
   );
 };
 
