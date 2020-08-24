1e225ff1797534a8e4f8895c15880d6a3f772af7
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-24 18:21:03 +1000

    Finished the App

--- app/javascript/packs/components/TodoForm.jsx
+++ app/javascript/packs/components/TodoForm.jsx
@@ -14,6 +14,7 @@ const TodoForm = props => {
         complete: false,
       },
     });
+    
     const todoItem = response.data;
     props.createTodoItem (todoItem);
     titleRef.current.value = '';
